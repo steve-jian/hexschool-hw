7d0473c5d04d85c96841d7b894ad585ddea7147d
steve5631 <alen2048@outlook.com>
2021-06-27 22:32:09 +0800

    add xeogl

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
-$(document).ready(function() {
-    $('.showmenu').on('click',  function(e){
-       e.preventDefault();
-       $('body').toggleClass('menu-show');
-   });
- });
+$(document).ready(function () {
+  $(".showmenu").on("click", function (e) {
+    e.preventDefault();
+    $("body").toggleClass("menu-show");
+  });
+});
+
+var scene = new xeogl.Scene({
+  canvas: "myCanvas",
+  transparent: true,
+});
+xeogl.setDefaultScene(scene);
+
+var kk = new xeogl.GLTFModel({
+  id: "kk",
+  src: "models/burger/burger_merged1.gltf",
+});
+
+kk.on("loaded", function () {
+  var cameraFlight = new xeogl.CameraFlightAnimation();
+  cameraFlight.flyTo(kk);
+});
+
+var cameraControl = new xeogl.CameraControl();
+
+scene.on("tick", function () {
+  var camera = scene.camera;
+  camera.orbitYaw(0.04);
+});
